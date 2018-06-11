fd0b1d77efc97f04f1b01e34dc93eba3feea04a2
amira2566 <braemira2@icloud.com>
2018-06-11 16:00:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,3 +7,14 @@ class Person
     puts "The Person is walking"
   end
 end
+
+# # dog.rb
+# class Dog
+#   def bark
+#   puts "Woof!"
+#   end
+#   def sit
+#   puts "The Dog is sitting"
+#   end
+# end
+  
